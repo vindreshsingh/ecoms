20625a81bfe5076b940eb118234e6d1e6ae34c32
vindreshsingh <vksingh5198@gmail.com>
2020-12-07 15:40:01 +0530

    Updates

---
+++ precache-manifest.238abae079e32aba4426d254ad0135fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32545505646b4ee97f8d4f4b16b32452",
+    "url": "/ecoms/index.html"
+  },
+  {
+    "revision": "5b52f6ec314468dc99d4",
+    "url": "/ecoms/static/css/main.1dd78cff.chunk.css"
+  },
+  {
+    "revision": "ae8b2bf149432bea3c56",
+    "url": "/ecoms/static/js/2.e5c7faa0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecoms/static/js/2.e5c7faa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b52f6ec314468dc99d4",
+    "url": "/ecoms/static/js/main.6dde722c.chunk.js"
+  },
+  {
+    "revision": "a8c39cc96d7b3f2a0636",
+    "url": "/ecoms/static/js/runtime-main.7d68ac98.js"
+  }
+]);
